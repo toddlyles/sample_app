2ad22bcf79494262876856997e42bbb7f0506088
Todd Lyles <toddlyles@gmail.com>
2012-05-22 08:04:24 -0500

    Everything through 5.3.3 of the tutorial

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,54 +3,54 @@ require 'spec_helper'
 describe "Static pages" do 
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_content('Sample App')
 		end
 
 		it "should have the right title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('title',
 				:text=>"Ruby on Rails Tutorial Sample App")
 		end
 
 		it "should not have a custom page title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should_not have_selector('title',:text=>'| Home')
 		end
 	end
 
   describe "Help page" do
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_content('Help')
 		end
 
 		it "should have the right title" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('title',:text=>"Help")
 		end
   end
 
   describe "About page" do
   		it "should have the content 'About Us'" do
-  			visit '/static_pages/about'
+  			visit about_path
   			page.should have_content('About Us')
   		end
 
   		it "should have the right title" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('title',:text=>"About")
 		end
   end
 
     describe "Contact" do
   		it "should have the content 'Contact Us'" do
-  			visit '/static_pages/contact'
+  			visit contact_path
   			page.should have_content('Contact Us')
   		end
 
   		it "should have the right title" do
-			visit '/static_pages/contact'
+			visit contact_path
 			page.should have_selector('title',:text=>"Contact Us")
 		end
   end
