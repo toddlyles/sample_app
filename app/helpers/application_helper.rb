5236d8ee44b528950986e561b65a8199fec3c6f9
Todd Lyles <toddlyles@gmail.com>
2012-05-21 19:03:18 -0500

    Add a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#returns the full title ona per-page basis
+	def full_title(page_title)
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if base_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,15 @@ describe "Static pages" do
 
 		it "should have the right title" do
 			visit '/static_pages/home'
-			page.should have_selector('title',:text=>"Home")
+			page.should have_selector('title',
+				:text=>"Ruby on Rails Tutorial Sample App")
 		end
 
-  end
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title',:text=>'| Home')
+		end
+	end
 
   describe "Help page" do
 		it "should have the content 'Help'" do
